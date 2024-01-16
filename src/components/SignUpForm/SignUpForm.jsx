28b6a52d012e250e7da35018847af2d76be55035
mouloud <mouloud619@gmail.com>
2024-01-16 00:03:46 -0600

    14 commit

---
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import { signUp } from "../../utilities/users-service";
+
+const SignUpForm = ({ setUser }) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirm: "",
+  });
+
+  const [error, setError] = useState("");
+
+  const handleChange = (evt) => {
+    setFormData({
+      ...formData,
+      [evt.target.name]: evt.target.value,
+    });
+    setError("");
+  };
+
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+    try {
+      const user = await signUp(formData);
+      setUser(user);
+    } catch {
+      setError("Sign Up Failed - Try Again");
+    }
+  };
+
+  const disable = formData.password !== formData.confirm;
+
+  return (
+    <div>
+      <div className="form-container">
+        <form autoComplete="off" onSubmit={handleSubmit}>
+          <label>Name</label>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+          <label>Confirm</label>
+          <input
+            type="password"
+            name="confirm"
+            value={formData.confirm}
+            onChange={handleChange}
+            required
+          />
+          <button type="submit" disabled={disable}>
+            SIGN UP
+          </button>
+        </form>
+      </div>
+      <p className="error-message">&nbsp;{error}</p>
+    </div>
+  );
+};
+
+export default SignUpForm;
