7f59180255af9319d8fb8359693f61a0359bebbb
mouloud <mouloud619@gmail.com>
2024-01-15 23:36:39 -0600

    8 commit

---
+++ config/database.js
@@ -0,0 +1,11 @@
+
+//database.js
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI);
+
+const db = mongoose.connection;
+
+db.on("connected", function () {
+  console.log(`Connected to ${db.name} at ${db.host}:${db.port}`);
+});
